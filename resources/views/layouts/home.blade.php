d1c4e8872397bc087ca1155f6d4c9ac7117e4e22
cmsbhathiya <bhathiya@eteamsys.lu>
2021-08-14 00:26:37 +0530

    google-tags

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,8 +11,19 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link href="{{ asset('/css/custom.css') }}" rel="stylesheet"> 
     @yield('css')
+        <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NSH4LP7');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
+ <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NSH4LP7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->   
 <header id="b-public-nav" class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
